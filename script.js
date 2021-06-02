c2b33bc6589e0ce1700ae295a983ef7370b572fa
Marcos Adriano <marcosadriano740@gmail.com>
2021-06-02 19:14:25 -0300

    Switch series fetch to parallel fetch

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
 const pokedexSection = document.querySelector('[data-pokedex]')
 
-// Get data from pokemons
-async function getData(number) {
+async function getPokemonData(number) {
   const pokemonData = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}/`)
   const data = await pokemonData.json()
   return data
 }
 
+async function getResolvedData(pokemonPromisesData) {
+  let pokemonData = []
+  for (let i = 0; i < pokemonPromisesData.length; i++) {
+    pokemonData.push(await pokemonPromisesData[i])
+  }
+  return pokemonData
+}
+
+function getDOMstructure(acc, cur) {
+  let types = cur.types.map(({ type }) => type.name)
+
+  return acc + `
+    <article class="${types.join(' ')}">
+      <img src="${cur.sprites.front_default}" alt="${cur.name}">
+      <h2 class="pokemon_name">${cur.name}</h2>
+      <span>Type: ${types.join(' | ')}</span>
+    </article>
+  `
+}
+
 async function init() {
-  let pokemons = []
-
-  for (let i = 1; i <=151; i++) {
-    let data = await getData(i)
-
-    let types = data.types.map(el => el.type.name)
-
-    pokemons.push(`
-      <article class="${types.join(' ')}">
-        <img src="${data.sprites.front_default}" alt="${data.name}">
-        <h2 class="pokemon_name">${data.name}</h2>
-        <span>Type: ${types.join(' | ')}</span>
-      </article>
-    `)
+  let pokemonPromisesData = []
+  for (let i = 1; i <= 151; i++) {
+    pokemonPromisesData.push(getPokemonData(i))
   }
 
-  pokedexSection.innerHTML = pokemons.join('')
+  const pokemonData = await getResolvedData(pokemonPromisesData)
+
+  pokedexSection.innerHTML = pokemonData.reduce(getDOMstructure, '')
 }
 
 init()
