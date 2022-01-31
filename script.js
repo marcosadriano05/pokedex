6319ee83015bbb8eceb8721b3daf8bd2a5b9fbbe
Marcos Adriano <71355331+marcosadriano05@users.noreply.github.com>
2022-01-31 16:17:17 -0300

    Increase performance (#1)
    * refactor: increase performance
    
    * refactor: extract functions

--- script.js
+++ script.js
@@ -1,40 +1,59 @@
 const pokedexSection = document.querySelector('[data-pokedex]')
 
-async function getPokemonData(number) {
-  const pokemonData = await fetch(`https://pokeapi.co/api/v2/pokemon/${number}/`)
-  const data = await pokemonData.json()
-  return data
+function parsePokemonData(pokemonData) {
+  const id = pokemonData.id
+  const name = pokemonData.name
+  const types = pokemonData.types.map(({ type }) => type.name)
+  const frontSprite = pokemonData.sprites.front_default
+
+  return { id, name, types, frontSprite }
 }
 
-async function getResolvedData(pokemonPromisesData) {
-  let pokemonData = []
-  for (let i = 0; i < pokemonPromisesData.length; i++) {
-    pokemonData.push(await pokemonPromisesData[i])
-  }
-  return pokemonData
+async function fetchPokemonDataPromise(number) {
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${number}/`)
 }
 
-function getDOMstructure(acc, cur) {
-  let types = cur.types.map(({ type }) => type.name)
-
-  return acc + `
-    <article class="${types.join(' ')}">
-      <img src="${cur.sprites.front_default}" alt="${cur.name}">
-      <h2 class="pokemon_name">${cur.name}</h2>
-      <span>Type: ${types.join(' | ')}</span>
+function pokemonCompoment(pokemonData) {
+  return `
+    <article class="${pokemonData.types.join(' ')} data-index=${pokemonData.id}">
+      <img src="${pokemonData.frontSprite}" alt="${pokemonData.name}">
+      <h2 class="pokemon_name">${pokemonData.name}</h2>
+      <span>Type: ${pokemonData.types.join(' | ')}</span>
     </article>
   `
 }
 
-async function init() {
-  let pokemonPromisesData = []
-  for (let i = 1; i <= 151; i++) {
-    pokemonPromisesData.push(getPokemonData(i))
+function getPokemonsPromisesData(quantity = 151) {
+  if (quantity > 151 || quantity < 0) {
+    throw new Error("Pokemons quantity must to be between 1 and 151")
   }
+  let pokemonPromisesData = []
+  for (let i = 1; i <= quantity; i++) {
+    pokemonPromisesData.push(fetchPokemonDataPromise(i))
+  }
+  return pokemonPromisesData
+}
 
-  const pokemonData = await getResolvedData(pokemonPromisesData)
+function sortPokemonsArray(pokemons) {
+  return pokemons.sort(function (a, b) {
+    if (a.id < a.id) return -1
+    if (a.id > b.id) return 1
+    return 0
+  })
+}
 
-  pokedexSection.innerHTML = pokemonData.reduce(getDOMstructure, '')
+async function resolveArrayOfPromises(promises) {
+  return await Promise.all(promises.map(async data => {
+    const pokemonData = await data.json()
+    return parsePokemonData(pokemonData)
+  }))
+}
+
+async function init() {
+  const pokemonsDataPromises = await Promise.all(getPokemonsPromisesData(151))
+  const pokemons = await resolveArrayOfPromises(pokemonsDataPromises)
+  const pokemonsSorted = sortPokemonsArray(pokemons)
+  pokedexSection.innerHTML = pokemonsSorted.reduce((acc, cur) => acc + pokemonCompoment(cur), '')
 }
 
 init()
